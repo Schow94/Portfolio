60e4185bc568f2657645ee7272a95f95cd41bd54
Schow94 <Schow94@gmail.com>
2019-10-10 21:55:45 -0700

    Updates

---
+++ precache-manifest.0a89ccc2a3c5a5d048fc1dbd3e87c1a7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33ef3c560adc745394a6117ecd5bb5d1",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "4b5fccba4771dc8fe1f1",
+    "url": "/Portfolio/static/js/2.450947e7.chunk.js"
+  },
+  {
+    "revision": "33877cf3736772dc8d32",
+    "url": "/Portfolio/static/js/main.45cefdfc.chunk.js"
+  },
+  {
+    "revision": "1cf07195fe355af74bc3",
+    "url": "/Portfolio/static/js/runtime-main.a69d7c3c.js"
+  },
+  {
+    "revision": "75f82b935bdd8e4dd2d50177efba049f",
+    "url": "/Portfolio/static/media/image_blog.75f82b93.png"
+  },
+  {
+    "revision": "92f0894052b593caebe102a6f8c6543e",
+    "url": "/Portfolio/static/media/image_calculator.92f08940.png"
+  },
+  {
+    "revision": "76488fc4bd7878e8974710ab4a57d5dd",
+    "url": "/Portfolio/static/media/image_emaily.76488fc4.png"
+  },
+  {
+    "revision": "5d1e6990bf74628848192dc8ab42fc31",
+    "url": "/Portfolio/static/media/image_magnitude.5d1e6990.png"
+  },
+  {
+    "revision": "cd9c8366f7940ccd12c56adb169d0536",
+    "url": "/Portfolio/static/media/image_reactNative2.cd9c8366.png"
+  },
+  {
+    "revision": "af7d42fc50ed453c6b36daaaae9b1181",
+    "url": "/Portfolio/static/media/image_shoppingCart.af7d42fc.png"
+  },
+  {
+    "revision": "3c493ed425ae370fb2b64e3e957bbd13",
+    "url": "/Portfolio/static/media/image_weather.3c493ed4.png"
+  },
+  {
+    "revision": "7f5fc5b9ae7f24ea44f07682ebc7e8a8",
+    "url": "/Portfolio/static/media/image_wifree.7f5fc5b9.png"
+  },
+  {
+    "revision": "7da2f594e5c09f67affcc9d7fb078ee1",
+    "url": "/Portfolio/static/media/logo_express_1.7da2f594.svg"
+  },
+  {
+    "revision": "2985235df31b8742d893d5b7e80145c5",
+    "url": "/Portfolio/static/media/logo_mongo.2985235d.svg"
+  },
+  {
+    "revision": "fdf7bec04423a85a896149949ac79083",
+    "url": "/Portfolio/static/media/logo_node.fdf7bec0.svg"
+  },
+  {
+    "revision": "05a3e30fdf60ebe9d00cf4725d44bd42",
+    "url": "/Portfolio/static/media/logo_react.05a3e30f.svg"
+  }
+]);
